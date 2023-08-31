461e418e20bc649314a5b05db9605eacfea23a7d
A-rielox <arielox.ag@gmail.com>
2023-08-30 19:11:52 -0600

    Add project files.

---
+++ WebApiAutores/Entidades/Autor.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApiAutores.Validaciones;
+
+namespace WebApiAutores.Entidades;
+
+public class Autor : IValidatableObject
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El campo {0} es requerido")]
+    [StringLength(maximumLength: 120, ErrorMessage = "El campo {0} no debe de tener más de {1} carácteres")]
+    [PrimeraLetraMayuscula]
+    public string Nombre { get; set; }
+
+    //[Range(18, 120)]
+    //[NotMapped]
+    //public int Edad { get; set; }
+    //[CreditCard]
+    //[NotMapped] ------> p' tener props que no van a estar en las tablas
+    //public string TarjetaDeCredito { get; set; }
+    //[Url]
+    //[NotMapped]
+    //public string URL { get; set; }
+
+    //public int Menor { get; set; }
+    //public int Mayor { get; set; }
+
+    public List<Libro> Libros { get; set; }
+
+
+    // validacion a nivel modelo
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(Nombre))
+        {
+            var primeraLetra = Nombre[0].ToString();
+
+            if (primeraLetra != primeraLetra.ToUpper())
+            {
+                yield return new ValidationResult("La primera letra debe ser mayúscula",
+                        new string[] { nameof(Nombre) }); // prop q tiene el error
+            }
+        }
+
+        //if (Menor > Mayor)
+        //{
+        //    yield return new ValidationResult("Este valor no puede ser más grande que el campo Mayor",
+        //        new string[] { nameof(Menor) });
+        //}
+    }
+}
---
+++ WebApiAutores/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using WebApiAutores.Servicios;
+
+namespace WebApiAutores;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    //////////////////////////////////////////
+    /////////////////////////////////////////////
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // AddJsonOptions p' evitar el error del cyclo al llamar entidades que se hacen referencia
+        // ( x las q creo los DTO's )
+        services.AddControllers().AddJsonOptions(x => 
+                    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
+
+        // cunado queran un IServicio => pasale un ServicioA
+        // servicioA requiere ILogger y se le va a pasar automaticamente TODAS sus dependencias
+        services.AddTransient<IServicio, ServicioA>(); 
+
+
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+
+}
